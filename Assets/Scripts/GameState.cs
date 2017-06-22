22afe195f646bc30085c80983fa23c4910465480
Megan Bowra-Dean <megan@megan.geek.nz>
2017-06-22 23:11:49 +1200

    A rotating turret.

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState : MonoBehaviour {
+
+    public static GameState instance = null;
+    public float currentPlayerAngle = 0;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Scripts/GameStateLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameStateLoader : MonoBehaviour {
+
+    public GameObject gameState;
+
+    private void Awake()
+    {
+        if (GameState.instance == null) Instantiate(gameState);
+    }
+}
